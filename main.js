ce2ea1e8afd3d6c13b787cd6d041910a2ac3c828
mohamedsahal <mohaansahal@gmail.com>
2022-04-17 10:27:05 +0300

    last

--- main.js
+++ main.js
@@ -4,10 +4,53 @@ let textArea = document.querySelector(".textarea");
 let notes = document.querySelector(".notes");
 let note = document.querySelector(".note");
 
+
+
+
 const hideNoteArea = () =>{
     textArea.style = "display: none";
     noteArea.classList.remove("note-now");
 }
+const getNotesFromStorage = ()=>{
+    let oldNote;
+    if(localStorage.getItem("notes") === null){
+        oldNote=[];
+    } else{
+        oldNote = JSON.parse(localStorage.getItem("notes"));
+    } oldNote.forEach(note => {
+        notes.innerHTML += `
+        <div class="note" id="note">
+        <h3 class="title-text" id="title-text">${note[0]}</h3>
+        <p class="note-blog">${note[1]}</p>
+        <i class="fa fa-trash"></i>
+    </div>
+        
+        `
+    });
+}
+const deleteFromStorage = (deletedNote) =>{
+    let oldNote;
+    if(localStorage.getItem("notes") === null){
+        oldNote=[];
+    } else{
+        oldNote = JSON.parse(localStorage.getItem("notes"));
+
+}
+oldNote.map((note, index)=>{
+    if(note[0] == deletedNote.children[0].textContenet.trim()  && note[1] == deletedNote.children[1].textContenet.trim() ){
+        oldNote.splice(index,1);
+        return oldNote;
+        
+
+        
+    }
+
+});
+    localStorage.setItem("notes", JSON.stringify(oldNote));
+  
+}
+
+document.addEventListener("DOMContentLoaded", getNotesFromStorage);
 
 const addNote = (t, n) =>{
     notes.innerHTML += `
@@ -22,14 +65,20 @@ const addNote = (t, n) =>{
 }
 
 const addNoteToLocalStorage = (note)=>{
-    if(note.length<0){
+    if(note.length <0){
         return;
     }
+    
 
     let oldNote;
-    if(localStorage.getItem("notes ") === null){
+    if(localStorage.getItem("notes") === null){
         oldNote=[];
+    } else{
+        oldNote = JSON.parse(localStorage.getItem("notes"));
     }
+    oldNote.push(note);
+    localStorage.setItem("notes", JSON.stringify(oldNote));
+
 }
 
 noteArea.addEventListener("click", () => {
@@ -83,5 +132,6 @@ document.addEventListener("mouseout", (event) =>{
  document.addEventListener("click", (event)=>{
      if(event.target.classList.contains("fa-trash")){
          event.target.parentElement.remove();
+         deleteFromStorage(event.target.parentElement);
      }
  })   
